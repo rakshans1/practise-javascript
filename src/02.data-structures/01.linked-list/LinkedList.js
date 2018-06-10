ee60dd5826b35c258a3456191a517b029fac2379
Rakshan Shetty <shetty.raxx555@gmail.com>
2018-06-10 20:29:35 +0530

    :recycle: Refactor

---
+++ src/02.data-structures/01.linked-list/LinkedList.js
@@ -0,0 +1,204 @@
+/*******************
+_        _           _                 _     _        _         _
+| |      (_)         | |               | |   | |      (_)       | |
+| |       _   _ __   | | __   ___    __| |   | |       _   ___  | |_
+| |      | | | '_ \  | |/ /  / _ \  / _` |   | |      | | / __| | __|
+| |____  | | | | | | |   <  |  __/ | (_| |   | |____  | | \__ \ | |_
+|______| |_| |_| |_| |_|\_\  \___|  \__,_|   |______| |_| |___/  \__|
+
+Big O Notation = O (n)
+
+*********************/
+
+import LinkedListNode from './LinkedListNode';
+
+/**
+ * @export
+ * @class LinkedList
+ */
+export default class LinkedList {
+  constructor() {
+    /** @var LinkedListNode */
+    this.head = null;
+
+    /** @var LinkedListNode */
+    this.tail = null;
+  }
+
+  /**
+   * @param {any} value
+   * @return {LinkedList}
+   * @memberof LinkedList
+   */
+  prepend(value) {
+    // Make new node to be a head.
+    this.head = new LinkedListNode(value, this.head);
+
+    return this;
+  }
+
+  /**
+   * @param {any} value
+   * @return {LinkedList}
+   * @memberof LinkedList
+   */
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    // If there is no head yet let's make new node a head.
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+  /**
+   * @param {any} value
+   * @returns {LinkedListNode}
+   * @memberof LinkedList
+   */
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+
+    // If the head must be deleted then make 2nd node to be a head.
+    if (this.head.value === value) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode !== null) {
+      // If next node must be deleted then make next node to be a next next one.
+      while(currentNode.next) {
+        if (currentNode.next.value === value) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+
+    // Check if tail must be deleted.
+    if (this.tail.value === value) {
+      deletedNode = null;
+      this.tail = currentNode;
+    }
+
+    return deletedNode;
+  }
+  /**
+   * @param {any} value
+   * @param {function} callback
+   * @returns {LinkedListNode}
+   * @memberof LinkedList
+   */
+  find({value = undefined, callback = undefined}) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while(currentNode.next) {
+      // If callback is specified then try to find node by callback.
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      // If value is specified then try to compare by value..
+      if (value !== undefined && currentNode.value === value) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+  /**
+   * @returns {LinkedListNode}
+   * @memberof LinkedList
+   */
+  deleteTail() {
+    if (this.head === this.tail) {
+      const deletedTail = this.tail;
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    const deletedTail  = this.tail;
+
+    // Rewind to the last node and delete "next" link for the node before the last one.
+    let currentNode = this.head;
+    while(currentNode.next) {
+      if(!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+    return deletedTail;
+  }
+
+  /**
+   * @returns {LinkedListNode}
+   * @memberof LinkedList
+   */
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+
+  /**
+   * @return {LinkedListNode[]}
+   */
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  /**
+   * @return {string}
+   * @memberof LinkedList
+   */
+  toString(callback) {
+    return this.toArray().map(node => node.toString(callback)).toString();
+  }
+}
+
