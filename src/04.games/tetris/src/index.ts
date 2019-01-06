c93bb6bcfa8b7106317f4509cf011302a9a62cad
Rakshan Shetty <shetty.raxx555@gmail.com>
2019-01-06 17:42:36 +0530

    Webpack Config Changed

--- src/04.games/tetris/src/index.ts
+++ src/04.games/tetris/src/index.ts
@@ -1,1 +1,2 @@
 import "./tetris";
+import "./style.css"
--- src/04.games/tetris/webpack.config.js
+++ src/04.games/tetris/webpack.config.js
@@ -1,23 +1,73 @@
-const path = require('path');
+const path = require("path");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: './src/index.ts',
-  mode: "development",
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
+const devMode = process.env.NODE_ENV !== "production";
+
+const webpack = {
+  common: {
+    entry: "./src/index.ts",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "build")
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html"
+      })
     ]
   },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+  development: {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: "./build"
+    },
+    plugins: [
+      new CleanWebpackPlugin(["build"]),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
   },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build')
+  production: {
+    mode: "production",
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].[hash].css",
+        chunkFilename: "[id].[hash].css"
+      })
+    ]
+  }
+};
+
+module.exports = () => {
+  const { common, development, production } = webpack;
+  if (devMode) {
+    return merge(common, development);
+  } else {
+    return merge(common, production);
   }
 };
